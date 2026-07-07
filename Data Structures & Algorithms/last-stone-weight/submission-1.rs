4d09a0101af2a078e309966ba9fb19b5b1806cf2
Longnhh37 <longnhh03@gmail.com>
2026-07-07 18:19:08 +0900

    Add: last-stone-weight - submission-1

---
+++ Data Structures & Algorithms/last-stone-weight/submission-1.rs
@@ -0,0 +1,16 @@
+use std::collections::BinaryHeap;
+
+impl Solution {
+    pub fn last_stone_weight(stones: Vec<i32>) -> i32 {
+        let mut heap = BinaryHeap::from(stones);
+        while heap.len() > 1 {
+            let a = heap.pop().unwrap();
+            let b = heap.pop().unwrap();
+            if a != b {
+                heap.push(a - b);
+            }
+        }
+
+        heap.pop().unwrap_or(0)
+    }
+}
