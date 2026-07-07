fbad4f0cc90132f79386026a3b1e04cb4ad6a017
Longnhh37 <longnhh03@gmail.com>
2026-07-07 19:04:53 +0900

    Add: maximum-subarray - submission-2

---
+++ Data Structures & Algorithms/maximum-subarray/submission-2.rs
@@ -0,0 +1,17 @@
+impl Solution {
+    pub fn max_sub_array(nums: Vec<i32>) -> i32 {
+        if nums.len() == 1 {
+            return nums[0];
+        }
+
+        let mut prev = nums[0];
+        let mut sum = nums[0];
+
+        for &cur in nums.iter().skip(1) {
+            prev = cur.max(cur + prev);
+            sum = sum.max(prev);
+        }
+
+        sum
+    }
+}
