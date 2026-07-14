c0efbc3b21dfb3a51736e1cb9fffe7529cc7f5b4
Longnhh37 <longnhh03@gmail.com>
2026-07-14 14:04:44 +0900

    Add: reorganize-string - submission-1

---
+++ Data Structures & Algorithms/reorganize-string/submission-1.rs
@@ -0,0 +1,41 @@
+use std::collections::BinaryHeap;
+
+impl Solution {
+    pub fn reorganize_string(s: String) -> String {
+        let mut res: Vec<u8> = Vec::with_capacity(s.len());
+        let mut prev: Option<(i32, u8)> = None;
+
+        let mut counter = [0_i32; 26];
+        for b in s.bytes() {
+            counter[(b - b'a') as usize] += 1;
+        }
+
+        let mut heap: BinaryHeap<(i32, u8)> = counter
+            .into_iter()
+            .enumerate()
+            .filter(|(_, cnt)| *cnt > 0)
+            .map(|(b, cnt)| (cnt, b as u8))
+            .collect();
+
+        loop {
+            if let Some((mut cnt, b)) = heap.pop() {
+                res.push(b'a' + b);
+                cnt -= 1;
+                if let Some(p) = prev.take() {
+                    heap.push(p);
+                }
+                if cnt > 0 {
+                    prev = Some((cnt, b));
+                }
+                
+            } else if prev.is_some() {
+                return String::new();
+
+            } else {
+                break;
+            }
+        }
+
+        String::from_utf8(res).unwrap()
+    }
+}
